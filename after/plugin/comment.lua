ee1efcabb58d3bffdbc626f548d7541ed5297e59
sjbavier <sjbavier@gmail.com>
2025-02-18 20:59:30 -0500

    start of neovim

---
+++ after/plugin/comment.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<C-_>", function() require('Comment.api').toggle.linewise.current() end, { noremap = true, silent = true })
+
--- lua/b4v/packer.lua
+++ lua/b4v/packer.lua
@@ -40,5 +40,11 @@ return require('packer').startup(function(use)
       'nvim-lualine/lualine.nvim',
       requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
+  }
 end)
 
