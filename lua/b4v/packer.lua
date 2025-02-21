bffd2991ab5466b90650488879125aa75b845cbe
sjbavier <sjbavier@gmail.com>
2025-02-20 20:46:14 -0500

    palenightfall

--- lua/b4v/packer.lua
+++ lua/b4v/packer.lua
@@ -14,11 +14,18 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  -- use({
+	-- 'rose-pine/neovim',
+	-- as = 'rose-pine',
+	-- config = function()
+		-- vim.cmd('colorscheme rose-pine')
+	-- end
+  -- })
   use({
-	'rose-pine/neovim',
-	as = 'rose-pine',
+	'JoosepAlviste/palenightfall.nvim',
+	as = 'palenightfall',
 	config = function()
-		vim.cmd('colorscheme rose-pine')
+		vim.cmd('colorscheme palenightfall')
 	end
   })
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -136,6 +136,12 @@ _G.packer_plugins = {
     path = "/home/b4v1n4t0r/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
+  palenightfall = {
+    config = { "\27LJ\2\n=\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\30colorscheme palenightfall\bcmd\bvim\0" },
+    loaded = true,
+    path = "/home/b4v1n4t0r/.local/share/nvim/site/pack/packer/start/palenightfall",
+    url = "https://github.com/JoosepAlviste/palenightfall.nvim"
+  },
   playground = {
     loaded = true,
     path = "/home/b4v1n4t0r/.local/share/nvim/site/pack/packer/start/playground",
@@ -145,12 +151,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/b4v1n4t0r/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
-  },
-  ["rose-pine"] = {
-    config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
-    loaded = true,
-    path = "/home/b4v1n4t0r/.local/share/nvim/site/pack/packer/start/rose-pine",
-    url = "https://github.com/rose-pine/neovim"
   },
   ["telescope.nvim"] = {
     loaded = true,
@@ -170,14 +170,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: rose-pine
-time([[Config for rose-pine]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
-time([[Config for rose-pine]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
+-- Config for: palenightfall
+time([[Config for palenightfall]], true)
+try_loadstring("\27LJ\2\n=\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\30colorscheme palenightfall\bcmd\bvim\0", "config", "palenightfall")
+time([[Config for palenightfall]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
